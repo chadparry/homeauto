52c4c75e40d500b1646effc471a97183af45e9f3
Chad Parry <github@chad.parry.org>
2016-01-16 16:30:39 -0800

    Rename a temp variable

--- at.py
+++ at.py
@@ -16,7 +16,7 @@ def schedule(when, cmd):
 			_AT_CMD + [local_when.strftime('%H:%M %d.%m.%Y')],
 			stdin=subprocess.PIPE,
 			stderr=subprocess.PIPE)
-	delayed_cmd = '; '.join(quote_command(cmd2) for cmd2 in (
+	delayed_cmd = '; '.join(quote_command(args) for args in (
 			_SLEEP_CMD + [local_when.strftime('%S.%f')],
 			cmd))
 	at.communicate(delayed_cmd)
